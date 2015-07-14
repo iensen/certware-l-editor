cfaebb37c866953a218c3e240346f45d9ad37e26
Matt Barry <mrb@softisms.com>
2015-07-13 21:04:56 -0500

    Better outline, filters

---
+++ net.certware.argument.language.ui/src/net/certware/argument/language/ui/outline/Messages.java
@@ -0,0 +1,23 @@
+package net.certware.argument.language.ui.outline;
+
+import org.eclipse.osgi.util.NLS;
+
+public class Messages extends NLS {
+	private static final String BUNDLE_NAME = "net.certware.argument.language.ui.outline.messages"; //$NON-NLS-1$
+	public static String FilterRuleContribution_0;
+	public static String FilterRuleContribution_1;
+	public static String FilterRuleContribution_2;
+	public static String FilterRuleContribution_3;
+	public static String FilterTypeDeclarationContribution_0;
+	public static String FilterTypeDeclarationContribution_1;
+	public static String FilterTypeDeclarationContribution_2;
+	public static String FilterTypeDeclarationContribution_3;
+	public static String FilterTypeDeclarationContribution_4;
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	}
+
+	private Messages() {
+	}
+}
